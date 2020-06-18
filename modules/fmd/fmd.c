73392e38b2763d0c2904f1cd63c60de9bf9b55ae
Thomas Adam <thomas@fvwm.org>
2020-06-18 01:29:07 +0100

    WIP

--- modules/fmd/fmd.c
+++ modules/fmd/fmd.c
@@ -11,7 +11,6 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/time.h>
-#include <sys/queue.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -21,6 +20,7 @@
 #include <errno.h>
 #include <err.h>
 #include <signal.h>
+#include <stdbool.h>
 
 #include <bson.h>
 #include <event.h>
@@ -39,6 +39,7 @@ static ModuleArgs *module;
 static int fvwm_fd[2];
 
 struct buffer;
+struct client;
 
 static int setnonblock(int);
 
@@ -54,6 +55,47 @@ static int write_to_fd(int, struct event *, struct buffer *);
 
 static void setup_signal_handlers(void);
 static RETSIGTYPE HandleTerminate(int sig);
+
+static int client_set_interest(struct client *, const char *);
+
+struct event_flag {
+	const char	*event;
+	int		 flag;
+} etf[] = {
+	{"new_window", M_ADD_WINDOW},
+	{"configure_window", M_CONFIGURE_WINDOW},
+	{"new_page", M_NEW_PAGE},
+	{"new_desk", M_NEW_DESK},
+	{"raise_window", M_RAISE_WINDOW},
+	{"lower_window", M_LOWER_WINDOW},
+	{"focus_change", M_FOCUS_CHANGE},
+	{"destroy_window", M_DESTROY_WINDOW},
+	{"iconify", M_ICONIFY},
+	{"deiconify", M_DEICONIFY},
+	{"window_name", M_WINDOW_NAME},
+	{"icon_name", M_ICON_NAME},
+	{"res_class", M_RES_CLASS},
+	{"res_name", M_RES_NAME},
+	{"icon_location", M_ICON_LOCATION},
+	{"map", M_MAP},
+	{"icon_file", M_ICON_FILE},
+	{"window_shade", M_WINDOWSHADE},
+	{"dewindow_shade", M_DEWINDOWSHADE},
+	{"restack", M_RESTACK},
+};
+
+static inline const char *
+flag_to_event(int flag)
+{
+	size_t	 i;
+
+	for (i = 0; i < (sizeof(etf) / sizeof(etf[0])); i++) {
+		if (etf[i].flag & flag)
+			return (etf[i].event);
+	}
+
+	return (NULL);
+}
 
 static RETSIGTYPE
 HandleTerminate(int sig)
@@ -164,9 +206,9 @@ broadcast_to_client(unsigned long type)
 	TAILQ_FOREACH(c, &clientq, entry) {
 		if (!(c->flags & type))
 			continue;
-		buf = fxstrdup("Got you a window");
+		asprintf(&buf, "Reacting to: %s", flag_to_event(type));
 
-		bufferq = fxcalloc(1, sizeof(*bufferq));
+		bufferq = fxmalloc(sizeof(*bufferq));
 		bufferq->len = strlen(buf);
 		bufferq->buf = fxstrdup(buf);
 		bufferq->offset = 0;
@@ -185,19 +227,11 @@ on_fvwm_read(int fd, short ev, void *arg)
 
 	if ((packet = ReadFvwmPacket(fd)) == NULL) {
 		fprintf(stderr, "Couldn't read from FVWM - exiting.\n");
-		close(fd);
-		event_del(&fvwm_read);
-
-		HandleTerminate(0);
+		goto done;
 	}
 
-	switch(packet->type) {
-	case M_ADD_WINDOW:
-		broadcast_to_client(packet->type);
-		break;
-	default:
-		break;
-	}
+	broadcast_to_client(packet->type);
+done:
 	SendUnlockNotification(fvwm_fd);
 }
 
@@ -207,12 +241,17 @@ on_fvwm_write(int fd, short ev, void *arg)
 	struct client 	*c = (struct client *)arg;
 	struct buffer	*b;
 
-	if (c == NULL)
+	if (c == NULL) {
+		fprintf(stderr, "%s: not continuing.  Client is NULL\n", __func__);
 		return;
+	}
 
-	b = TAILQ_FIRST(&c->f_writeq);
+	TAILQ_FOREACH(b, &c->f_writeq, entries) {
+		fprintf(stderr, "Sent: <<%s>>\n", b->buf);
+		SendText(fvwm_fd, b->buf, 0);
+	}
 
-	if (b != NULL && (write_to_fd(fd, &fvwm_write, b)) == 0) {
+	TAILQ_FOREACH(b, &c->f_writeq, entries) {
 		/* Write complete.  Remove from the buffer. */
 		TAILQ_REMOVE(&c->f_writeq, b, entries);
 		free(b->buf);
@@ -220,11 +259,50 @@ on_fvwm_write(int fd, short ev, void *arg)
 	}
 }
 
+static inline bool
+strsw(const char *pre, const char *str)
+{
+	return (strncmp(pre, str, strlen(pre)) == 0);
+}
+
+#define EFLAGSET	0x1
+#define EFLAGUNSET	0x2
+
+static int
+client_set_interest(struct client *c, const char *event)
+{
+	size_t		i;
+	int		flag_type;
+	bool		changed = false;
+#define PRESET "set"
+#define PREUNSET "unset"
+
+	if (strsw(PRESET, event)) {
+		event += strlen(PRESET) + 1;
+		flag_type = EFLAGSET;
+	} else if (strsw(PREUNSET, event)) {
+		event += strlen(PREUNSET) + 1;
+		flag_type = EFLAGUNSET;
+	}
+
+	for (i = 0; i < (sizeof(etf) / sizeof(etf[0])); i++) {
+		if (strcmp(etf[i].event, event) == 0) {
+			changed = true;
+			if (flag_type == EFLAGSET)
+				c->flags |= etf[i].flag;
+			else
+				c->flags &= ~etf[i].flag;
+		}
+	}
+
+	return (changed);
+}
+
 static void
 on_client_read(int fd, short ev, void *arg)
 {
 	struct client 	*client = (struct client *)arg;
-	struct buffer	*bufferq;
+	struct buffer	*bufferq, *b;
 	char		*buf;
 	int		 len;
 
@@ -236,6 +314,12 @@ on_client_read(int fd, short ev, void *arg)
 		close(fd);
 		event_del(&client->read);
 		TAILQ_REMOVE(&clientq, client, entry);
+
+		TAILQ_FOREACH(b, &client->f_writeq, entries) {
+			TAILQ_REMOVE(&client->f_writeq, b, entries);
+			free(b->buf);
+			free(b);
+		}
 		free(client);
 		return;
 	}
@@ -249,11 +333,8 @@ on_client_read(int fd, short ev, void *arg)
 		return;
 	}
 
-	if (strcmp(buf, "set new_window") == 0) {
-		client->flags |= M_ADD_WINDOW;
-
+	if (client_set_interest(client, buf))
 		return;
-	}
 
 	/* Anything else is sent through to FVWM3. */
 	bufferq = fxcalloc(1, sizeof(*bufferq));
@@ -264,6 +345,8 @@ on_client_read(int fd, short ev, void *arg)
 
 	free(buf);
 
+	event_del(&fvwm_write);
+	event_set(&fvwm_write, fvwm_fd[0], EV_WRITE, on_fvwm_write, client);
 	event_add(&fvwm_write, NULL);
 }
 
@@ -304,7 +387,7 @@ on_client_accept(int fd, short ev, void *arg)
 	if (setnonblock(client_fd) < 0)
 		fprintf(stderr, "couldn't make socket non-blocking\n");
 
-	client = fxcalloc(1, sizeof(*client));
+	client = fxmalloc(sizeof(*client));
 
 	event_set(&client->read, client_fd, EV_READ|EV_PERSIST, on_client_read,
 	    client);
@@ -321,6 +404,8 @@ on_client_accept(int fd, short ev, void *arg)
 	TAILQ_INIT(&client->c_writeq);
 	TAILQ_INIT(&client->f_writeq);
 	TAILQ_INSERT_TAIL(&clientq, client, entry);
+
+	event_add(&fvwm_write, NULL);
 
 	fprintf(stderr, "Accepted connection...\n");
 }
@@ -349,6 +434,11 @@ main(int argc, char **argv)
 	/* Set up the read/write listeners as early as possible, as there's
 	 * every chance FVWM will send us information as soon as we start up.
 	 */
+	if (setnonblock(fvwm_fd[0]) < 0)
+		err(1, "fvwm to_fvwm socket non-blocking failed");
+	if (setnonblock(fvwm_fd[1]) < 0)
+		err(1, "fvwm from_fvwm socket non-blocking failed");
+
 	event_set(&fvwm_read, fvwm_fd[1], EV_READ|EV_PERSIST, on_fvwm_read, NULL);
 	event_add(&fvwm_read, NULL);
 
